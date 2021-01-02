82523a9ac139fedc3735b6448d55d3f096a0ac6b
a0k00xa <ambreen.khan@walmart.com>
2021-01-01 20:59:27 -0500

    fix styling

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <MemeGenerator />
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,10 +5,6 @@ class Header extends React.Component {
     render(){
         return(
             <header className="header-mem">
-                <img 
-                src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" 
-                alt="Problem?"
-                />
                 <p>Meme Generator</p>
             </header>
         )
